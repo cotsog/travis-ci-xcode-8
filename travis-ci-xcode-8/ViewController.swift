927d8d802257d450ed42376c57f128e7f8b43534
Lennart Stolz <lennart.stolz@westwing.de>
2016-08-24 10:04:06 +0200

    Initial commit.

---
+++ travis-ci-xcode-8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  travis-ci-xcode-8
+//
+//  Created by Lennart Stolz on 24/08/16.
+//  Copyright © 2016 Lennart Stolz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ travis-ci-xcode-8Tests/travis_ci_xcode_8Tests.swift
@@ -0,0 +1,36 @@
+//
+//  travis_ci_xcode_8Tests.swift
+//  travis-ci-xcode-8Tests
+//
+//  Created by Lennart Stolz on 24/08/16.
+//  Copyright © 2016 Lennart Stolz. All rights reserved.
+//
+
+import XCTest
+@testable import travis_ci_xcode_8
+
+class travis_ci_xcode_8Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
